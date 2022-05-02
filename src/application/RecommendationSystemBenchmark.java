0fef751aed3cab2d72cb69d877472afbd2e85e4a
Gabriel Reis Andrade <joaogabriel2@gmail.com>
2022-05-02 20:08:07 -0300

    Changed microbenchmark

--- src/application/RecommendationSystemBenchmark.java
+++ src/application/RecommendationSystemBenchmark.java
@@ -1,28 +1,16 @@
 package application;
 import java.util.HashMap;
-import java.util.concurrent.TimeUnit;
-
 import org.openjdk.jmh.annotations.Benchmark;
-import org.openjdk.jmh.annotations.BenchmarkMode;
 import org.openjdk.jmh.annotations.Fork;
-import org.openjdk.jmh.annotations.Measurement;
-import org.openjdk.jmh.annotations.Mode;
-import org.openjdk.jmh.annotations.OutputTimeUnit;
 import org.openjdk.jmh.annotations.Scope;
 import org.openjdk.jmh.annotations.Setup;
 import org.openjdk.jmh.annotations.State;
-import org.openjdk.jmh.annotations.Warmup;
-
 import fileReader.FileReader;
 import fileReader.JsonFileReader;
 import model.Movie;
 import model.User;
 import recommendation.ItemRecommendation;
 @State(Scope.Benchmark)
-@BenchmarkMode(Mode.Throughput)
-@Warmup(iterations = 5, time = 1, timeUnit = TimeUnit.SECONDS)
-@Measurement(iterations = 5, time = 1, timeUnit = TimeUnit.SECONDS)
-@OutputTimeUnit(TimeUnit.SECONDS)
 @Fork(1)
 public class RecommendationSystemBenchmark {
 	private static HashMap<Integer,User> users;
