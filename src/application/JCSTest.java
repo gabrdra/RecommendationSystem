18ddb36257a42a6a92685c8b849c7010fe54f285
Gabriel Reis Andrade <joaogabriel2@gmail.com>
2022-06-08 15:33:14 -0300

    Fixed pom and created skeletal jcstress test class

---
+++ src/application/JCSTest.java
@@ -0,0 +1,19 @@
+package application;
+
+import org.openjdk.jcstress.annotations.Actor;
+import org.openjdk.jcstress.annotations.Expect;
+import org.openjdk.jcstress.annotations.JCStressTest;
+import org.openjdk.jcstress.annotations.Outcome;
+import org.openjdk.jcstress.annotations.State;
+import org.openjdk.jcstress.infra.results.II_Result;
+
+import recommendation.ItemRecommendation;
+
+public class JCSTest {
+
+	@State
+	public static class IRState extends ItemRecommendation{}
+	
+	
+	
+}
